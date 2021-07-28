fac769ccecf2a547c6293e7712edb8b4f89f43cf
alexsandroalexandrino <sandrooliveira1501@gmail.com>
2021-07-28 12:38:15 -0300

    Script for generating instances

---
+++ Instances/run.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+operations = [0.1, 0.3, 0.5, 1]
+qtd = 1000
+
+for n in range(10, 501, 50):
+	for op in operations:
+		for model in ["r","t","rt"]:
+			file = "input/%s_%s_%s.in" % (model, n, op)
+			command = "python generate_instance %s %s %s %s > %s" % (qtd, n, int(n*op), model, file)
+			print(command)
+			# os.system(command)
